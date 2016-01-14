e704f741ccc1e0f114bfeba727d74d8a60c66eea
Vitaliy Babiychuk <bvvnbsp@gmail.com>
2016-01-14 04:35:35 +0200

    Reconfig ga script.

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,17 +1,10 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', gon.ga_id, 'auto', {userId: gon.current_session_id});
-ga('require', 'displayfeatures');
-ga('send', 'pageview');
-
 var _gaq = _gaq || [];
+_gaq.push(['_setAccount', gon.ga_id]);
 _gaq.push(['_trackPageview']);
 
-setTimeout(function() {
+(function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-}, 0);
+})();
+
